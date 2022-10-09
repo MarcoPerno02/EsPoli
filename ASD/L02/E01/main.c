ff062de679e50ec283a5feba9810ab330fa245c2
MarcoPerno02 <86968490+MarcoPerno02@users.noreply.github.com>
2022-10-09 12:46:34 +0200

    s

---
+++ ASD/L02/E01/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int a, int b);
+
+int main() {
+    int a, b;
+    printf("Inserisci a: ");
+    scanf("%d", &a);
+    printf("Inserisci b: ");
+    scanf("%d", &b);
+    
+    if(b > a)
+    {
+        int aus = a;
+        a = b;
+        b = aus;   
+    }
+    printf("%d", gcd(a, b));
+}
+
+int gcd(int a, int b)
+{   
+    if(a == 0 || b == 0) {
+        return 1;
+    }  
+    if(a % 2 == 0 && b % 2 == 0)
+    {
+        return 2 * gcd(a/2, b/2);
+    }
+    else if(a % 2 != 0 && b % 2 != 0)
+    {
+        return gcd((a-b)/2, b);
+    }
+    else if(a % 2 != 0 && b % 2 == 0)
+    {
+        return gcd(a, b/2);
+    }
+    else
+    {
+        int resto;
+        while (b > 0)	{
+            resto = a % b;
+            a = b;
+            b = resto;
+        }
+        return a;
+    }
+}
